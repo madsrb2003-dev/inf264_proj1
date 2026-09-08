fd51a7d4d02acc14e7c33a085cb553e6717aa8f4
Mads Bolstad <mabol8965@uib.no>
2026-09-08 11:46:41 +0200

    entropy

--- decision_tree.py
+++ decision_tree.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 class DecisionTree: 
     def __init__(self, criterion, max_depth, root):
@@ -6,38 +7,21 @@ class DecisionTree:
         self.max_depth = max_depth
         self.root = None
 
-
-    def identical_features(X):
-
-        first_row = X[0]
-
-        for row in X:
-            for index in range(len(row)):
-                if row[index] != first_row[index]:
-                    return False
-
-        return True
-
-
     def fit(self, X, y):
 
-        first_leaf = y[0]
-
         # Step 1: Check if all labels are the same
-        for i in range(y): 
-            if i != first_leaf:
-                break
+        if identical_labels(y):
+            return y[0]
 
-        else: return first_leaf # Doesn't matter which leaf
-
+        
         # Step 2: Check if all datapoints have the same features
-
-        if self.identical_features(X):
+        if identical_features(X):
             most_common_label = np.bincount(y).argmax()
 
             return most_common_label
 
-        
+
+        # Step 3: Calculate information gain
 
         
 
@@ -46,3 +30,36 @@ class DecisionTree:
         # IMPLEMENT
         pass
 
+
+
+# Helper methods 
+
+def decision_entropy(y):
+    counts = np.bincount(y)
+    decision_zero = (counts[0]/np.sum(counts)) * math.log2(counts[0]/np.sum(counts))
+    decision_one = (counts[1]/np.sum(counts)) * math.log2(counts[1]/np.sum(counts))
+
+    entropy = -(decision_zero + decision_one)
+
+    return entropy
+
+
+def identical_labels(y):
+    first_label = y[0]
+
+    for label in y:
+        if label != first_label:
+            return False
+        
+    return True
+
+def identical_features(X):
+
+    first_row = X[0]
+
+    for row in X:
+        for index in range(len(row)):
+            if row[index] != first_row[index]:
+                return False
+
+    return True
